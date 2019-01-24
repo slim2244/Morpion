4dcb231ca5cb5022fd7601f717fa81908176597f
slim2244 <45916883+slim2244@users.noreply.github.com>
2019-01-24 12:57:47 +0100

    Add files via upload

---
+++ intro.rb
@@ -0,0 +1,24 @@
+class Intro
+
+def menu
+puts "Pour jouer au morpion, il vous faut :"
+puts " "
+puts " "
+
+puts "Etre 2 joueurs"
+puts " "
+puts " "
+puts "Comment jouer au morpion ?"
+puts " "
+puts " "
+puts "Pour jouer une partie de morpion, il suffit de tracer sur une feuille blanche une grille de 3 cases sur 3 (selon les variantes, il est possible d’augmenter le nombre de cases). Le but du jeu est d’aligner avant son adversaire 3 symboles identiques horizontalement, verticalement ou en diagonale."
+puts "Chaque joueur a donc son propre symbole, généralement une croix pour l’un et un rond pour l’autre. La partie se termine quand l’un des joueurs à aligner 3 symboles ou quand la grille est complétée sans vainqueur. Il y a alors égalité."
+
+puts "Comment gagner une partie de Morpion ?"
+puts " "
+puts " "
+puts "Le premier joueur a aligner 3 symboles identiques gagne la partie. Attention, le joueur qui débute est toujours avantagé pour gagner. Pensez donc à alterner !"
+
+	end 
+
+end 
